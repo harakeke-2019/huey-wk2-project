96b0af1cf3c1b90b9d49ac2e1b1d46a3a9e36276
Lane Wirihana le Prevost-Smith <lanelps@gmail.com>
2019-01-18 15:40:53 +1300

    refactoring

--- route.js
+++ route.js
@@ -61,28 +61,7 @@ router.get('/styles/3000', (req, res) => {
       console.log(contentsObj)
    })
 })
-// router.post('/styles/:id', (req, res) => {
-//    let filepath =""
-//    const id = Number(req.params.id)
-//    const messsage = req.body.name
-//    let messageObj={}
-//    if (id === 1950){
-//       filepath = path.join(__dirname, './data1950.json')
-//    }
-//    else if (id === 1980){
-//          filepath  = path.join(__dirname, './data1980.json')
-//    }
 
-//    else {
-//       filepath = path.join(__dirname, './data3000.json')
-//    }
-//    fs. readFile(filepath, (err, contents) =>{
-//       if (err) res.status(500).send('file access error')
-//       const contentsObj = JSON.parse(contents)
-//          res.render('index', contentsObj) 
-//    })
-
-// })   
 router.get('/styles/blank', (req, res) =>{
    const filepath = path.join(__dirname, './datablank.json')
    fs. readFile(filepath, (err, contents) =>{
